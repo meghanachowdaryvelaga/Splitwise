d2db7a45568a51f952280f788fbd6f644b90c605
Kousik kumar Vasireddy <kousikkumarv@gmail.com>
2025-11-27 00:06:48 -0600

    user modifications

--- src/components/add-user/add-user.ts
+++ src/components/add-user/add-user.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-add-user',
-  imports: [MatCardModule,MatButtonModule],
+  imports: [MatCardModule,MatButtonModule,MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatIconModule],
   templateUrl: './add-user.html',
   styleUrl: './add-user.css',
 })
 export class AddUser {
-
+value = 'Meghana';
 }
