fc303e5158bc9a5c51d5954c6304188e3a7bd9d2
meghanachowdaryvelaga <MeghanaChowdaryVelaga@my.unt.edu>
2025-12-05 12:15:39 -0600

    changes made in card

--- src/components/user-list/user-list.ts
+++ src/components/user-list/user-list.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AddUser } from "../add-user/add-user";
 import { CommonModule } from '@angular/common';
+import { MatCardContent, MatCard, MatCardHeader, MatCardTitle } from "@angular/material/card";
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [AddUser, CommonModule],
+  imports: [AddUser, CommonModule, MatCardContent, MatCard, MatCardHeader, MatCardTitle],
   templateUrl: './user-list.html',
   styleUrls: ['./user-list.css'],
 })
