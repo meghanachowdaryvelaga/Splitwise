46acbfcd1f98183898521b8df5f4a4247ba1e31c
meghanachowdaryvelaga <MeghanaChowdaryVelaga@my.unt.edu>
2025-12-10 14:11:16 -0600

    changes done in show-users-list branch

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddUser } from "../components/add-user/add-user";
 import { UserList } from '../components/user-list/user-list';
+import { HomePage } from "../components/home-page/home-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AddUser, UserList],
+  imports: [RouterOutlet, AddUser, UserList, HomePage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/components/home-page/home-page.ts
+++ src/components/home-page/home-page.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { UserList } from "../user-list/user-list";
+import { AddUser } from "../add-user/add-user";
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [UserList, AddUser],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
 export class HomePage {
 
+  user: string[] = [];
+  onUserAdded(username: string) {
+    this.user.push(username);
+  } 
 }
