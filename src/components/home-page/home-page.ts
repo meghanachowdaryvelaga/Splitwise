1f77fa6f5346fa168de13b88e858dcbe3f920d42
meghanachowdaryvelaga <MeghanaChowdaryVelaga@my.unt.edu>
2026-01-12 17:00:55 -0600

    final chnages

--- src/components/home-page/home-page.ts
+++ src/components/home-page/home-page.ts
@@ -3,17 +3,19 @@ import { UserList } from "../user-list/user-list";
 import { AddUser } from "../add-user/add-user";
 import { UserInterface } from '../../models/user.interface';
 import { MatTabsModule } from '@angular/material/tabs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [UserList, AddUser, MatTabsModule],
+  imports: [UserList, AddUser, MatTabsModule, FormsModule],
   templateUrl: './home-page.html',
   styleUrls: ['./home-page.css'],
 })
 export class HomePage {
 
   user: UserInterface[] = [];
+  searchtext: string = '';
   onUserAdded(username: UserInterface) {
     this.user.push(username);
   } 
