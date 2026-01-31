30a66fe34d0c1c563e060f2927769b54056367e1
meghanachowdaryvelaga <143774163+meghanachowdaryvelaga@users.noreply.github.com>
2026-01-30 18:44:49 -0600

    element data added as observable

--- src/shared-service.ts
+++ src/shared-service.ts
@@ -1,5 +1,25 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { BehaviorSubject, of, Subject } from 'rxjs';
+
+export interface PeriodicElement {
+  name: string;
+  srNo: number;
+  youOwe: number;
+  owesToYou: string;
+}
+
+export const ELEMENT_DATA: PeriodicElement[] = [
+  {srNo: 1, name: 'Hydrogen', youOwe: 1.0079, owesToYou: 'H'},
+  {srNo: 2, name: 'Helium', youOwe: 4.0026, owesToYou: 'He'},
+  {srNo: 3, name: 'Lithium', youOwe: 6.941, owesToYou: 'Li'},
+  {srNo: 4, name: 'Beryllium', youOwe: 9.0122, owesToYou: 'Be'},
+  {srNo: 5, name: 'Boron', youOwe: 10.811, owesToYou: 'B'},
+  {srNo: 6, name: 'Carbon', youOwe: 12.0107, owesToYou: 'C'},
+  {srNo: 7, name: 'Nitrogen', youOwe: 14.0067, owesToYou: 'N'},
+  {srNo: 8, name: 'Oxygen', youOwe: 15.9994, owesToYou: 'O'},
+  {srNo: 9, name: 'Fluorine', youOwe: 18.9984, owesToYou: 'F'},
+  {srNo: 10, name: 'Neon', youOwe: 20.1797, owesToYou: 'Ne'}
+];
 
 @Injectable({
   providedIn: 'root',
@@ -7,4 +27,7 @@ import { BehaviorSubject, Subject } from 'rxjs';
 export class SharedService {
   isAddExpensesTabDisabled = new Subject<boolean>();
   applicationTitle = new BehaviorSubject<string>('Splitwise');
+  getData(){
+    return of(ELEMENT_DATA);
+  }
 }
