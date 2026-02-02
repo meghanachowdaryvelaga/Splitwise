279b1aeefb0841409f46f6702dd8df202c75eedc
meghanachowdaryvelaga <143774163+meghanachowdaryvelaga@users.noreply.github.com>
2026-02-02 11:43:27 -0600

    unsunscribed

--- src/components/user-list/user-list.ts
+++ src/components/user-list/user-list.ts
@@ -1,9 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { AddUser } from "../add-user/add-user";
 import { CommonModule } from '@angular/common';
 import { UserInterface } from '../../models/user.interface';
 import { MatTableModule } from '@angular/material/table';
 import { SharedService, PeriodicElement } from '../../shared-service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
 @Component({
   selector: 'app-user-list',
   standalone: true,
@@ -11,16 +14,25 @@ import { SharedService, PeriodicElement } from '../../shared-service';
   templateUrl: './user-list.html',
   styleUrls: ['./user-list.css'],
 })
-export class UserList implements OnInit {
+export class UserList implements OnInit,OnDestroy {
   @Input() users: UserInterface[] = [];
   displayedColumns: string[] = ['srNo', 'name', 'youOwe', 'owesToYou'];
   dataSource: PeriodicElement[] = [];
+  destroy$ = new Subject<void>();
 
   constructor(private sharedService: SharedService) {}
+  
   ngOnInit(){
-    this.sharedService.getData().subscribe((res)=>{
+    console.log('subscribing to getData()');
+    this.sharedService.getData().pipe(takeUntil(this.destroy$)).subscribe((res)=>{
+      console.log('received data', res);
       this.dataSource=res;
-    })
+    });
+  }
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+    console.log('user-list: ngOnDestroy - emitting destroy$');
   }
   
   onUserAdded(username: UserInterface) {
