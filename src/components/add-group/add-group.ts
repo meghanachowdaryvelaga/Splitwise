f452b8e80f1a9fb3ac1b65aca04aadefce883c12
meghanachowdaryvelaga <143774163+meghanachowdaryvelaga@users.noreply.github.com>
2026-02-19 20:36:48 -0600

    feat(add groups): :sparkles: adding a group
    User can able to create groups to share bills for trip or any event

--- src/components/add-group/add-group.ts
+++ src/components/add-group/add-group.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { UserInterface } from '../../models/user.interface';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-add-group',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatIconModule, FormsModule, MatInputModule,CommonModule, MatCheckboxModule],
   templateUrl: './add-group.html',
   styleUrl: './add-group.css',
 })
 export class AddGroup {
-
+value='';
+@Output() groupAdded= new EventEmitter<UserInterface>();
+addGroup(){
+  const name=this.value.trim();
+  if(!name){
+    alert("Please add group to proceed");
+    return;
+  }
+  const group:UserInterface={
+    name:name,
+    youOwe:0,
+    owesToYou:0
+  };
+  this.groupAdded.emit(group);
+  this.value = '';
+}
 }
